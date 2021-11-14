ef6c0ae2ee7f451422f9f9513ba8ceca2b75b2e6
Ilya <ilya.panshenskov@italki.com>
2021-11-14 11:48:27 +0800

    adding the model

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import keras
 from keras.models import model_from_json
 from keras.preprocessing.sequence import pad_sequences
 
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+predictor = model_from_json(loaded_model_json)
+
 app = Flask(__name__)
 
 @app.route("/")
