cf1dfc0ce0b77398c04d2f952161e6a05a4d75f6
Ilya <ilya.panshenskov@italki.com>
2021-11-14 11:27:10 +0800

    adding html page

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'Hi there!'
+    return render_template('page.html')
 
 if __name__  == '__main__': 
     app.run(debug=True)
